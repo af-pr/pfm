fb17a5928f1d8268d1c4dd1f6571ea35aa49fd35
AF-PR <alejandrof.platero@hotmail.com>
2026-03-27 18:01:16 +0100

    test: configure unit and integration test phases

---
+++ finance/finance-app/src/test/java/com/afpr/pfm/finance/ApplicationIT.java
@@ -0,0 +1,13 @@
+package com.afpr.pfm.finance;
+
+import com.afpr.pfm.finance.config.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+class ApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
