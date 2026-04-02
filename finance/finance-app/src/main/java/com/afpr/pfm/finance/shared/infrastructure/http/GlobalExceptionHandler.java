a62b4ad946d9504a5b64f08042d32c98c9304b6e
af-pr <alejandrof.platero@hotmail.com>
2026-04-02 11:31:52 +0200

    chore(logs): add HTTP and captured error logs to the app

--- finance/finance-app/src/main/java/com/afpr/pfm/finance/shared/infrastructure/http/GlobalExceptionHandler.java
+++ finance/finance-app/src/main/java/com/afpr/pfm/finance/shared/infrastructure/http/GlobalExceptionHandler.java
@@ -2,21 +2,25 @@ package com.afpr.pfm.finance.shared.infrastructure.http;
 
 import com.afpr.pfm.finance.shared.exception.ConflictException;
 import com.afpr.pfm.finance.shared.exception.NotValidException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<Void> handleConflict(ConflictException ex) {
+        log.error("Conflict error occurred", ex);
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
     @ExceptionHandler(NotValidException.class)
     public ResponseEntity<Void> handleNotValid(NotValidException ex) {
+        log.error("Not valid error occurred", ex);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
